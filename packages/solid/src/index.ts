cf0858ac9c53d38f864862552d7bb8ff1aa7b71b
jaxoncreed <jaxoncreed@gmail.com>
2024-02-24 11:32:48 -0500

    Fix tests and build

--- packages/solid/src/index.ts
+++ packages/solid/src/index.ts
@@ -18,3 +18,12 @@ export * from "./requester/requests/readResource";
 export * from "./requester/requests/requestOptions";
 export * from "./requester/requests/updateDataResource";
 export * from "./requester/requests/uploadResource";
+
+export * from "./resource/wac/WacRule";
+export * from "./resource/wac/getWacRule";
+export * from "./resource/wac/getWacUri";
+export * from "./resource/wac/setWacRule";
+export * from "./resource/wac/results/GetWacRuleSuccess";
+export * from "./resource/wac/results/GetWacUriSuccess";
+export * from "./resource/wac/results/SetWacRuleSuccess";
+export * from "./resource/wac/results/WacRuleAbsent";
