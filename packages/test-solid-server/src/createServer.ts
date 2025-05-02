5b7982cbced009584c6aebf6e792e1bb9c8e6cb4
Jackson Morgan <jaxoncreed@gmail.com>
2025-05-02 14:06:58 -0400

    Filled out the test-solid-server library

---
+++ packages/test-solid-server/src/createServer.ts
@@ -0,0 +1,37 @@
+// Taken from https://github.com/comunica/comunica/blob/b237be4265c353a62a876187d9e21e3bc05123a3/engines/query-sparql/test/QuerySparql-solid-test.ts#L9
+
+import * as path from "path";
+import type { App } from "@solid/community-server";
+import { AppRunner, resolveModulePath } from "@solid/community-server";
+import "jest-rdf";
+
+// Use an increased timeout, since the CSS server takes too much setup time.
+jest.setTimeout(40_000);
+
+export async function createApp(
+  port: number,
+  customConfigPath?: string,
+): Promise<App> {
+  if (process.env.SERVER) {
+    return {
+      start: () => {},
+      stop: () => {},
+    } as App;
+  }
+  const appRunner = new AppRunner();
+
+  return appRunner.create({
+    loaderProperties: {
+      mainModulePath: resolveModulePath(""),
+      typeChecking: false,
+    },
+    config: customConfigPath ?? resolveModulePath("config/file-root.json"),
+    variableBindings: {},
+    shorthand: {
+      port: port,
+      loggingLevel: "off",
+      seedConfig: path.join(__dirname, "configs", "solid-css-seed.json"),
+      rootFilePath: path.join(__dirname, "./data"),
+    },
+  });
+}
