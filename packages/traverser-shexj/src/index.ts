760b5f7eb0470928977fbbf92af826e7b5355ce7
Jackson Morgan <jaxoncreed@gmail.com>
2025-05-16 15:35:56 -0400

    esm migration for schema-converter-shex

--- packages/traverser-shexj/src/index.ts
+++ packages/traverser-shexj/src/index.ts
@@ -2,4 +2,5 @@ import { ShexJTraverser } from "./ShexJTraverser.js";
 
 export * from "./ShexJTraverserDefinition.js";
 export * from "./ShexJTraverserTypes.js";
+export * from "./ShexJTypes.js";
 export default ShexJTraverser;
