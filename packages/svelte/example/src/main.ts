3ea1c58642e3294d70d7b310290d2e2a359f2049
Jackson Morgan <jaxoncreed@gmail.com>
2025-05-19 10:53:53 -0400

    Removed test for prepublishonly

--- packages/svelte/example/src/main.ts
+++ packages/svelte/example/src/main.ts
@@ -1,9 +1,9 @@
 import App from "./App.svelte";
 // You might have some global CSS for the example app itself
-// import './app.css'; 
+// import './app.css';
 
 const app = new App({
-  target: document.getElementById('app')!,
+  target: document.getElementById("app")!,
 });
 
 export default app;
