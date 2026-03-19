5984674585a5ca9d8d9885c532f1e15795fae1b7
Jackson Morgan <jaxoncreed@gmail.com>
2026-03-19 00:16:05 -0400

    Fixed tsdown accross all packages

--- packages/solid-react/tsdown.config.ts
+++ packages/solid-react/tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
+  target: "es2021",
   unbundle: true,
   dts: true,
   clean: true,
